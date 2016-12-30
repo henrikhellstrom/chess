30961d8270c550eb240fcca57bec0526059a6158
Henrik Hellstrom <henriktempa@gmail.com>
2016-12-30 11:49:43 +0100

    Added pawn implementation

--- gameState.py
+++ gameState.py
@@ -1,15 +1,16 @@
 from piece import Piece
+from pawn import Pawn
+
 class GameState:
     pieces = []
 
     def __init__(self):
-        self.init_test_pieces()
+        self.init_white_pawns()
 
-    def init_test_pieces(self):
-        piece = Piece([0, 0])
-        self.pieces.append(piece)
-        piece = Piece([2, 5])
-        self.pieces.append(piece)
+    def init_white_pawns(self):
+        for x in range(0, 8):
+            pawn = Pawn(True, [x, 6])
+            self.pieces.append(pawn)
 
     def draw_pieces(self, surface):
         for piece in self.pieces:
--- piece.py
+++ piece.py
@@ -17,4 +17,7 @@ class Piece:
             pixel_pos[1] = self.pos[1]*constants.SQUARE_SIZE + constants.SQUARE_SIZE/2
             pygame.draw.circle(surface, self.yellow, pixel_pos, constants.SQUARE_SIZE/3)
         else:
-            pass
+            pixel_pos[0] = self.pos[0]*constants.SQUARE_SIZE
+            pixel_pos[1] = self.pos[1]*constants.SQUARE_SIZE
+            rect = pygame.Rect(pixel_pos[0], pixel_pos[1], constants.SQUARE_SIZE, constants.SQUARE_SIZE)
+            surface.blit(self.image, rect)
