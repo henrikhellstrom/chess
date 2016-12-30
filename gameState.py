cb3fe4b9b1b6a2ee91b4965b6ee5fd3eba5bad65
Henrik Hellstrom <henriktempa@gmail.com>
2016-12-30 23:10:18 +0100

    Begun adding support for pawn movement

--- gameState.py
+++ gameState.py
@@ -5,12 +5,15 @@ class GameState:
     pieces = []
 
     def __init__(self):
-        self.init_white_pawns()
+        self.init_pawns()
 
-    def init_white_pawns(self):
+    def init_pawns(self):
         for x in range(0, 8):
-            pawn = Pawn(True, [x, 6])
-            self.pieces.append(pawn)
+            white_pawn = Pawn(True, [x, 6])
+            self.pieces.append(white_pawn)
+            black_pawn = Pawn(False, [x, 1])
+            self.pieces.append(black_pawn)
+
 
     def draw_pieces(self, surface):
         for piece in self.pieces:
--- pawn.py
+++ pawn.py
@@ -3,9 +3,24 @@ import pygame
 
 class Pawn(Piece):
     #white is a boolean
+    #pos is a list [x, y], holding square index
     def __init__(self, white, pos):
         if white == True:
             self.image = pygame.image.load("white_pawn.png")
         else:
             self.image = pygame.image.load("black_pawn.png")
         self.pos = pos
+
+    #Returns which moves would be possible on an empty board
+    def moves(self):
+        ret = []
+        if self.white == True:
+            if self.pos[1]-1 >= 0:
+                ret.append([self.pos[0], self.pos[1]-1])
+            if self.pos[1] == 6:
+                ret.append([self.pos[0], self.pos[1]-2])
+        if self.white == False:
+            if self.pos[1]+1 <= 7:
+                ret.append([self.pos[0], self.pos[1]+1])
+            if self.pos[1] == 1:
+                ret.append([self.pos[0], self.pos[1]+2])
